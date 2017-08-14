c38ac0513987afddc7e955702cb7075b2efc6ed1
ciarancourtney <ciaran.courtney@cylon.com>
2017-08-14 21:29:20 +0100

    revert _build_app change (Travis' docker must be to blame)

--- frameworks/C++/includeos-mana/_build_app.sh
+++ frameworks/C++/includeos-mana/_build_app.sh
@@ -1,4 +1,5 @@
 echo "Building IncludeOS Mana server"
 rm -fr build
 mkdir build
-docker run --rm -v $(dirname $PWD):/build/service includeos/includeos-build:0.10.0.1
+cd build
+docker run --rm -v $(dirname $PWD):/service includeos/includeos-build:0.10.0.1
