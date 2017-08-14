011ea879073c8cc3e15ce3bf1b00e2cf016e56df
ciarancourtney <ciaran.courtney@cylon.com>
2017-08-14 19:23:16 +0100

    Fix _build_app.sh, docker run path should be /build

--- frameworks/C++/includeos-mana/_build_app.sh
+++ frameworks/C++/includeos-mana/_build_app.sh
@@ -1,6 +1,4 @@
 echo "Building IncludeOS Mana server"
 rm -fr build
 mkdir build
-cd build
-docker run --rm -v $(dirname $PWD):/service includeos/includeos-build:0.10.0.1
-cd ..
+docker run --rm -v $(dirname $PWD):/build/service includeos/includeos-build:0.10.0.1
