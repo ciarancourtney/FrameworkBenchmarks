a8261882119fc28a40d51e939554291ab2c22379
ciarancourtney <ciaran.courtney@cylon.com>
2017-08-16 14:05:21 +0100

    Update setup.sh

--- frameworks/OCaml/mirageos/setup.sh
+++ frameworks/OCaml/mirageos/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 OCAML_VER='4.03.0'
+MIRAGE_VER='3.0.5'
 
 UBUNTU_VER=$(lsb_release -sr | cut -d'.' -f1)
 expr $UBUNTU_VER
@@ -10,7 +11,7 @@ sudo apt-get update
 sudo apt-get install -yqq m4
 
 echo "Installing OPAM (takes >10min)"
-if (( $UBUNTU_VER >= 16 )); then
+if (( $UBUNTU_VER >= 16 )); then  # Travis Ubuntu 14.04 support
     sudo apt-get install -yqq opam
 else
     sudo add-apt-repository -y ppa:avsm/ppa
@@ -26,5 +27,14 @@ opam switch $OCAML_VER
 echo "    Setting up compiler env"
 eval `opam config env`
 
-echo "Installing MirageOS"
-opam install -y mirage=3.0.2
+echo "Installing MirageOS $MIRAGE_VER"
+opam install -y mirage=$MIRAGE_VER
+
+echo "Building MirageOS Conduit HTTP Server"
+# based on https://github.com/mirage/mirage-skeleton/tree/master/device-usage/conduit_server
+mirage configure -t unix --net socket
+make depend
+make
+
+echo "Booting MirageOS Conduit HTTP Server"
+./conduit_server
