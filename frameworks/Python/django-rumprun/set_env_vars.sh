ef46c67445f8e919675e18447571b4d361dd983f
ciarancourtney <ciaran.courtney@cylon.com>
2017-08-17 21:06:36 +0100

    main.py + boilerplate setup + travis + py362

---
+++ frameworks/Python/django-rumprun/set_env_vars.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+export PY3_ROOT=$HOME/FrameworkBenchmarks/installs/py3
--- frameworks/Python/django-rumprun/setup.sh
+++ frameworks/Python/django-rumprun/setup.sh
@@ -1,6 +1,54 @@
 #!/bin/bash
 
-fw_depends mysql python2
+
+
+#fw_depends mysql python3
+
+set -e
+
+sudo apt-get install -yqq python3-dev genisoimage
+
+rm -rf venv
+$PY3_ROOT/bin/python3 -m venv venv
+
+sudo apt-get install -yqq libmysqlclient-dev
+
+sudo apt-get install -yqq libpq-dev
+
+# uncomment if running on wsl https://github.com/Microsoft/BashOnWindows/issues/1043
+#sudo apt-get install grub-pc-bin
+
+
+venv/bin/pip install -r requirements.txt
+
+echo "Installing rumpkernel rumprun"
+git clone http://repo.rumpkernel.org/rumprun
+cd rumprun
+git submodule update --init
+./build-rr.sh hw
+
+export PATH="${PATH}:$(pwd)/rumprun/bin"
+
+cd py3-rump
+make
+cd ..
+
+genisoimage -r -o main.iso main.py venv/lib/python3.5/site-packages
+
+rumprun-bake hw_generic python.bin py3-rump/build/python
+
+sudo ip tuntap add tap0 mode tap
+sudo ip addr add 10.0.120.100/24 dev tap0
+sudo ip link set dev tap0 up
+
+rumprun kvm -i \
+	-I if,vioif,'-net tap,ifname=tap0,script=no' \
+        -W if,inet,static,10.0.120.101/24 \
+	-b py3-rump/images/python.iso,/python/lib/python3.5 \
+	-b main.iso,/python/lib/python3.5/site-packages \
+	-e PYTHONHOME=/python \
+        -- python.bin -m main
+
 
 pip install --install-option="--prefix=${PY2_ROOT}" -r $TROOT/requirements.txt
 
